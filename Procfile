b13a4678a029ad0ce92064c54f6b749713702a95
scardenasb <84429399+scardenasb@users.noreply.github.com>
2022-03-31 05:25:20 -0300

    bug:Procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn tutorial.wsgi --log-file -
+web: gunicorn config.wsgi --log-file -
--- config/urls.py
+++ config/urls.py
@@ -20,5 +20,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(r'home/', include('urlshortener.urls')),
+    path('', include('urlshortener.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
