7e35ebec1aba9fcc5c9a709fc3416eb5585a8514
scardenasb <84429399+scardenasb@users.noreply.github.com>
2022-03-31 06:20:38 -0300

    bug module

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
 web: gunicorn config.wsgi --log-file -
-web: python manage.py runserver
--- urlshortener/views.py
+++ urlshortener/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from .models import Shortener
 from .forms import ShortenerForm
-import requests
 
 # Create your views here.
 
