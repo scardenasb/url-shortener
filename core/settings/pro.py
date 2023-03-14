b3fc53ce77c5bcd5633a75fc3a6391e91cd870cc
scardenasb <84429399+scardenasb@users.noreply.github.com>
2023-03-13 22:57:47 -0300

    update

--- core/settings/pro.py
+++ core/settings/pro.py
@@ -11,12 +11,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-import environ
 import os
 from dotenv import load_dotenv
 
-env = environ.Env(
-)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
