c11c75f1521f5a20af5baa8b708673c75431a466
scardenasb <84429399+scardenasb@users.noreply.github.com>
2023-03-14 00:12:16 -0300

    update

--- core/settings/pro.py
+++ core/settings/pro.py
@@ -94,8 +94,8 @@ DATABASES = {
         'NAME': os.environ['DB_NAME'],
         'USER': os.environ['DB_USER'],
         'PASSWORD': os.environ['DB_PASSWORD'],
-        'HOST': os.environ['DV_HOST'],
-        'PORT': os.environ['DV_PORT'],
+        'HOST': os.environ['DB_HOST'],
+        'PORT': os.environ['DB_PORT'],
     }
 }
 
