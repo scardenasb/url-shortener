70a74e0b2fbf0ef539aa38b7a151ab7fcbcd3dbe
scardenasb <84429399+scardenasb@users.noreply.github.com>
2023-05-03 22:31:37 -0700

    update

--- core/settings/pro.py
+++ core/settings/pro.py
@@ -133,6 +133,8 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = "/static/"
+STATICFILSE_DIRS = os.path.join(BASE_DIR, 'static')
+#STATICFILES_DIRS = "/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
