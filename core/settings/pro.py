ae57765ea979c62a6dbaa62739fb4bd9290d7334
scardenasb <84429399+scardenasb@users.noreply.github.com>
2023-05-03 22:19:05 -0700

    update

--- core/settings/pro.py
+++ core/settings/pro.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ['DEBUG'])
+DEBUG = bool(int(os.environ['DEBUG']))
 
 ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS = ['https://*.railway.app','https://*.127.0.0.1']
